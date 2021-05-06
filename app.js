fd997829a03612769c3299f1c84e9dc95df97fa3
Christian Barcelona <christianbarcelona95@gmail.com>
2021-05-06 23:43:12 +0100

    added responsive navigation bar

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert ('hello world my name is Christian Barcelona!');
+const navToggle = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navToggle();
