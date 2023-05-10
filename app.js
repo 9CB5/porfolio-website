ba12f60cebe96b2dd43dc3086fa66532e1048fea
Christian Barcelona <christianbarcelona95@gmail.com>
2023-05-10 11:35:53 +0100

    finish refactor

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let width = window.innerWidth;
+
 function navSetup() {
     const burger = document.querySelector(".navigation__burger");
     const links = document.querySelector(".navigation__links");
@@ -9,6 +11,20 @@ function navSetup() {
         overlay.classList.toggle("show");
         body.classList.toggle("scroll-lock");
     });
+}
+
+function linkClicked() {
+    const body = document.querySelector("body");
+    const links = document.querySelector(".navigation__links");
+    const overlay = document.querySelector(".navigation__overlay");
+
+    console.log(width);
+
+    if (width < 1440) {
+        body.classList.toggle("scroll-lock");
+        links.classList.toggle("show");
+        overlay.classList.toggle("show");
+    }
 }
 
 function main() {
